28981ddc98f72afb068a31040175fa0c6dbc3fe9
Wong Yu Hang <cosmowong2023@gmail.com>
2024-02-02 17:45:08 +0800

    third

---
+++ calculator.js
@@ -0,0 +1,42 @@
+let name = document.getElementById("id")
+
+function numberOnClick() {
+    console.log(this);
+}
+document.addEventListener('DOMContentLoaded', function(){
+    let digitBtns = document.getElementsByClassName('digit');
+
+    for ( let i=0; i<digitBtns.length; i++) {
+        digitBtns[i].addEventListener('click',numberOnClick)
+    }
+}
+)
+
+
+let myClass = document.querySelector(".myclass");
+let digitBtn = document.querySelector("div#calculator p.digit.zero");
+let digitBtns = document.querySelectorAll("div#calculator p.digit");
+
+function id(id) {
+    return document.getElementById(id);
+ }
+ 
+ function qs(selector) {
+   return document.querySelector(selector);
+ }
+ 
+ function qsa(selector) {
+   return document.querySelectorAll(selector);
+ }
+
+ function operatorOnClick() {
+    console.log(this);
+}
+document.addEventListener('DOMContentLoaded', function(){
+    let digitBtns = document.getElementsByClassName('operators');
+
+    for ( let i=0; i<digitBtns.length; i++) {
+        digitBtns[i].addEventListener('click',operatorOnClick)
+    }
+}
+)
