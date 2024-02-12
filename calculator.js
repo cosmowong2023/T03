ee8c78d7b5e824765906629a3f4a6d6b3c66322f
Wong Yu Hang <cosmowong2023@gmail.com>
2024-02-12 04:23:37 +0000

    j

--- calculator.js
+++ calculator.js
@@ -1,16 +1,18 @@
-let newList = [1]
+let newList = ["1"]
 
 function numberOnClick() {
-    console.log(this);
+  console.log(this);
+  let magicNum = this;
+  newList += magicNum
+  displayValue.innerHTML = newList;
 }
-document.addEventListener('DOMContentLoaded', function(){
-    let digitBtns = document.getElementsByClassName('digit');
+document.addEventListener('DOMContentLoaded', function() {
+  let digitBtns = document.getElementsByClassName('digit');
 
-    for ( let i=0; i<digitBtns.length; i++) {
-        digitBtns[i].addEventListener('click',numberOnClick)
-    }
-}
-)
+  for (let i = 0; i < digitBtns.length; i++) {
+    digitBtns[i].addEventListener('click', numberOnClick)
+  }
+})
 
 
 let myClass = document.querySelector(".myclass");
@@ -18,28 +20,28 @@ let digitBtn = document.querySelector("div#calculator p.digit.zero");
 let digitBtns = document.querySelectorAll("div#calculator p.digit");
 
 function id(id) {
-    return document.getElementById(id);
- }
- 
- function qs(selector) {
-   return document.querySelector(selector);
- }
- 
- function qsa(selector) {
-   return document.querySelectorAll(selector);
- }
-
- function operatorOnClick() {
-    console.log(this);
+  return document.getElementById(id);
 }
-document.addEventListener('DOMContentLoaded', function(){
-    let digitBtns = document.getElementsByClassName('operators');
 
-    for ( let i=0; i<digitBtns.length; i++) {
-        digitBtns[i].addEventListener('click',operatorOnClick)
-    }
+function qs(selector) {
+  return document.querySelector(selector);
 }
-)
 
-let outputValue= document.getElementById("displayValue");
-newList.innerHTML=outputValue;
+function qsa(selector) {
+  return document.querySelectorAll(selector);
+}
+
+function operatorOnClick() {
+  console.log(this);
+}
+document.addEventListener('DOMContentLoaded', function() {
+  let digitBtns = document.getElementsByClassName('operators');
+
+  for (let i = 0; i < digitBtns.length; i++) {
+    digitBtns[i].addEventListener('click', operatorOnClick)
+  }
+})
+
+let outputValue = document.getElementById("displayValue");
+
+displayValue.innerHTML = newList;
