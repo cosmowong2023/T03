157dc720fb145b1c04c2eff170799414a705637f
Cosmo Wong <cosmowong2023@gmail.com>
2024-02-29 04:29:22 +0800

    hi

--- calculator.js
+++ calculator.js
@@ -3,30 +3,33 @@ let numberClick = true
 let inputValue = 0
 let dotted = false
 let previous_operator = ""
+let Math_error = false
+
 
 function numberOnClick() {
-  if (numberClick==true)
-  {
-    if (newList!="0")
+  if (Math_error==false){
+    if (numberClick==true)
     {
-      console.log(this);
-      let magicNum = this.innerHTML;
-      let outputValue = document.getElementById('displayValue');
-      newList += magicNum;
-      outputValue.innerHTML = newList;
-      numberClick = true;
+      if (newList!="0")
+      {
+        console.log(this);
+        let magicNum = this.innerHTML;
+        let outputValue = document.getElementById('displayValue');
+        newList += magicNum;
+        outputValue.innerHTML = newList;
+        numberClick = true;
+      }
+      if (newList=="0")
+      {
+        console.log(this);
+        let magicNum = this.innerHTML;
+        let outputValue = document.getElementById('displayValue');
+        newList = magicNum;
+        outputValue.innerHTML = newList;
+        numberClick = true;
+      }
     }
-    if (newList=="0")
-    {
-      console.log(this);
-      let magicNum = this.innerHTML;
-      let outputValue = document.getElementById('displayValue');
-      newList = magicNum;
-      outputValue.innerHTML = newList;
-      numberClick = true;
-    }
-  }
-  else {
+    else {
     newList=""
     console.log(this);
     let magicNum = this.innerHTML;
@@ -34,6 +37,7 @@ function numberOnClick() {
     newList += magicNum;
     outputValue.innerHTML = newList;
     numberClick = true;
+    }
   }
 }
 document.addEventListener('DOMContentLoaded', function() {
@@ -45,8 +49,22 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 function dotOnClick() {
-  if (numberClick==true)
-  {
+  if (Math_error==false){
+    if (numberClick==true)
+    {
+        console.log(this);
+        let inputdot = this.innerHTML;
+        let outputValue = document.getElementById('displayValue');
+        if (dotted==false)
+        {
+          newList += inputdot;
+          outputValue.innerHTML = newList;
+          numberClick = true;
+        }
+    }
+    else
+    {
+      newList="0"
       console.log(this);
       let inputdot = this.innerHTML;
       let outputValue = document.getElementById('displayValue');
@@ -57,18 +75,6 @@ function dotOnClick() {
         numberClick = true;
       }
   }
-  else
-  {
-    newList="0"
-    console.log(this);
-    let inputdot = this.innerHTML;
-    let outputValue = document.getElementById('displayValue');
-    if (dotted==false)
-    {
-      newList += inputdot;
-      outputValue.innerHTML = newList;
-      numberClick = true;
-    }
 }  
   dotted = true
   return dotted, numberClick
@@ -94,21 +100,69 @@ function qsa(selector) {
 }
 
 function operatorOnClick() {
-  if (numberClick==true)
+if (Math_error==false)
   {
+    if (numberClick==true)
+    {
       console.log(this);
       let inputoperator = this.innerHTML;
       let Value = document.getElementById('displayValue');
-      inputValue = Value.innerHTML
+      let NewValue = Value.innerHTML
       console.log(inputoperator)
-      console.log(inputValue)
-      if (numberClick==true)
-      { 
-        previous_operator = inputoperator;
-
-        
+      console.log(NewValue)
+      
+      { if (previous_operator == "+")
+          {
+            let result = parseFloat(inputValue) + parseFloat(NewValue);
+            previous_operator = inputoperator;
+            Value.innerHTML = ""+result
+            inputValue=result
+            numberClick=false
+          }
+        else if (previous_operator == "-")
+          {
+            let result = parseFloat(inputValue) - parseFloat(NewValue);
+            previous_operator = inputoperator;
+            Value.innerHTML = ""+result
+            inputValue=result
+            numberClick=false
+          }
+        else if (previous_operator == "x")
+          {
+            let result = parseFloat(inputValue) * parseFloat(NewValue);
+            previous_operator = inputoperator;
+            Value.innerHTML = ""+result
+            inputValue=result
+            numberClick=false
+          }
+        else if (previous_operator == "/")
+          {  if (parseFloat(NewValue) !=0)
+            {
+              let result = parseFloat(inputValue) / parseFloat(NewValue);
+              previous_operator = inputoperator;
+              Value.innerHTML = ""+result
+              inputValue=result
+              numberClick=false
+            }
+            else
+            { let result = "Math Error!";
+              Value.innerHTML = ""+result
+              Math_error=true
+            }
+          }
+        else
+          {
+            previous_operator = inputoperator;
+            inputValue=NewValue;
+            numberClick=false;
+          }
       }
-      numberClick = false;
+    }
+    else
+    {
+      let inputoperator = this.innerHTML;
+      previous_operator = inputoperator;
+    }
   }
   dotted = false
   return dotted, numberClick
@@ -130,7 +184,9 @@ function clearOnClick(){
   newList = 0;
   outputValue.innerHTML = newList;
   numberClick = true;
-  dotted = false
+  dotted = false;
+  Math_error=false;
+  inputValue=0;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
