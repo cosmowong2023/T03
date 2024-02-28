131e00b05f7a2b6b54081e3cf4f654df890de6e4
Cosmo Wong <cosmowong2023@gmail.com>
2024-02-29 00:33:59 +0800

    hi

--- calculator.js
+++ calculator.js
@@ -1,5 +1,6 @@
 let newList = [""]
 let numberClick = true
+let inputValue = 0
 
 function numberOnClick() {
   if (numberClick==true)
@@ -9,18 +10,18 @@ function numberOnClick() {
       console.log(this);
       let magicNum = this.innerHTML;
       let outputValue = document.getElementById('displayValue');
-      newList += magicNum
+      newList += magicNum;
       outputValue.innerHTML = newList;
-      numberClick = true
+      numberClick = true;
     }
     if (newList=="0")
     {
       console.log(this);
       let magicNum = this.innerHTML;
       let outputValue = document.getElementById('displayValue');
-      newList = magicNum
+      newList = magicNum;
       outputValue.innerHTML = newList;
-      numberClick = true
+      numberClick = true;
     }
   }
 }
@@ -32,7 +33,38 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 })
 
+function dotOnClick() {
+  if (numberClick==true)
+  {
+    if ("." in newList)
+    {
+      console.log(this);
+      let inputdot = this.innerHTML;
+      let outputValue = document.getElementById('displayValue');
+      newList += inputdot;
+      outputValue.innerHTML = newList;
+      numberClick = true;
+    }
+    else
+    {
+      console.log(this);
+      let inputdot = this.innerHTML;
+      let outputValue = document.getElementById('displayValue');
+      newList += inputdot;
+      outputValue.innerHTML = newList;
+      numberClick = true;
+    }
+  }
+}
 
+document.addEventListener('DOMContentLoaded', function() {
+  let digitBtns = document.getElementsByClassName('dot');
+  let dotv = this.innerHTML;
+  let inputValue = document.getElementById('displayValue');
+  for (let i = 0; i < digitBtns.length; i++) {
+    digitBtns[i].addEventListener('click', operatorOnClick)
+  }
+})
 
 function id(id) {
   return document.getElementById(id);
@@ -52,13 +84,9 @@ function operatorOnClick() {
 document.addEventListener('DOMContentLoaded', function() {
   let digitBtns = document.getElementsByClassName('operators');
   let read_operator = this.innerHTML;
-  let inputValue = document.getElementById('displayValue');
-  
-  
+  inputValue = document.getElementById('displayValue');
   for (let i = 0; i < digitBtns.length; i++) {
     digitBtns[i].addEventListener('click', operatorOnClick)
-  
   }
-
 })
 
